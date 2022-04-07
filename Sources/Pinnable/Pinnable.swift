b508308e334a8d0003a5d55ce4756fed836d0efc
Steven Magdy <stevenmagdy92@gmail.com>
2022-04-07 17:17:21 +0200

    Remove redundant calls

--- Sources/Pinnable/Pinnable.swift
+++ Sources/Pinnable/Pinnable.swift
@@ -37,10 +37,10 @@ extension Pinnable {
         to object: P,
         insets: UIEdgeInsets = .zero
     ) -> (top: NSLayoutConstraint?, leading: NSLayoutConstraint?, bottom: NSLayoutConstraint?, trailing: NSLayoutConstraint?) {
-        let top = edges.contains(.top) ? topAnchor.pin(to: object.topAnchor, constant: insets.top) : nil
-        let leading = edges.contains(.left) ? leadingAnchor.pin(to: object.leadingAnchor, constant: insets.left) : nil
-        let bottom = edges.contains(.bottom) ? bottomAnchor.pin(to: object.bottomAnchor, constant: -insets.bottom) : nil
-        let trailing = edges.contains(.right) ? trailingAnchor.pin(to: object.trailingAnchor, constant: -insets.right) : nil
+        let top = edges.contains(.top) ? topAnchor.constraint(equalTo: object.topAnchor, constant: insets.top) : nil
+        let leading = edges.contains(.left) ? leadingAnchor.constraint(equalTo: object.leadingAnchor, constant: insets.left) : nil
+        let bottom = edges.contains(.bottom) ? bottomAnchor.constraint(equalTo: object.bottomAnchor, constant: -insets.bottom) : nil
+        let trailing = edges.contains(.right) ? trailingAnchor.constraint(equalTo: object.trailingAnchor, constant: -insets.right) : nil
 
         disableTranslatesAutoresizingMaskIntoConstraintsIfNeeded()
         NSLayoutConstraint.activate([top, leading, bottom, trailing].compactMap { $0 })
@@ -58,8 +58,8 @@ extension Pinnable {
         to object: P,
         offset: UIOffset = .zero
     ) -> (x: NSLayoutConstraint, y: NSLayoutConstraint) {
-        let centerX = centerXAnchor.pin(to: object.centerXAnchor, constant: offset.horizontal)
-        let centerY = centerYAnchor.pin(to: object.centerYAnchor, constant: offset.vertical)
+        let centerX = centerXAnchor.constraint(equalTo: object.centerXAnchor, constant: offset.horizontal)
+        let centerY = centerYAnchor.constraint(equalTo: object.centerYAnchor, constant: offset.vertical)
 
         disableTranslatesAutoresizingMaskIntoConstraintsIfNeeded()
         NSLayoutConstraint.activate([centerX, centerY])
@@ -79,8 +79,8 @@ extension Pinnable {
     @discardableResult public func pinSize<P: Pinnable>(
         to object: P
     ) -> (width: NSLayoutConstraint, height: NSLayoutConstraint) {
-        let height = heightAnchor.pin(to: object.heightAnchor)
-        let width = widthAnchor.pin(to: object.widthAnchor)
+        let height = heightAnchor.constraint(equalTo: object.heightAnchor)
+        let width = widthAnchor.constraint(equalTo: object.widthAnchor)
 
         disableTranslatesAutoresizingMaskIntoConstraintsIfNeeded()
         NSLayoutConstraint.activate([height, width])
@@ -100,8 +100,8 @@ extension Pinnable {
     @discardableResult public func pinSize(
         to size: CGSize
     ) -> (width: NSLayoutConstraint, height: NSLayoutConstraint) {
-        let height = heightAnchor.pin(to: size.height)
-        let width = widthAnchor.pin(to: size.width)
+        let height = heightAnchor.constraint(equalToConstant: size.height)
+        let width = widthAnchor.constraint(equalToConstant: size.width)
 
         disableTranslatesAutoresizingMaskIntoConstraintsIfNeeded()
         NSLayoutConstraint.activate([height, width])
